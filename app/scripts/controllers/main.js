ebac5e36ccf8ca12040fd94563833176b419f9d3
Jose Moreno <jose.moreno@reyesic.net>
2015-01-03 17:29:27 +0000

    Fetch from the API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,40 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name workspaceApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the workspaceApp
- */
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.queries = function(tag){
+            var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent";
+            var request = {
+                client_id: "f2cbd0eb30e44f3a97ac27a001bcd2f8",
+                callback: "JSON_CALLBACK" 
+            };
+
+            $http({
+                method: 'JSONP',
+                url: url,
+                params: request
+            }).
+            success(function(result) {
+                $scope.pictures = result.data;
+                $scope.searching = false;
+                $scope.done = true;
+                console.log($scope.pictures);
+            }).
+            error(function() {
+                console.log('error');
+            });
+        };
+        
+        $scope.searching = false;
+        $scope.done = false;
+        $scope.tag = "";
+        $scope.search = function(tag) {
+            console.log(tag);
+            if($scope.instaSearch.$valid) {
+               $scope.master= angular.copy(tag);
+               $scope.tag="";
+               $scope.searching = true;
+               $scope.queries(tag);
+            }
+         };
+    });
